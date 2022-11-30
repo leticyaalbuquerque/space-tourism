3128287d854e6618fc9c8bd25d25635428359049
Leticia <leticyaalbuquerque@outlook.com>
2022-11-30 18:05:27 -0300

    Inclusão pasta dist

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </>
