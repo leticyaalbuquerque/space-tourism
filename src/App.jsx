dfe97c1ae419191e6fd69d5c70ab161e5662abae
Leticia <leticyaalbuquerque@outlook.com>
2022-11-30 19:01:10 -0300

    Alteração na url dos links do header

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ export default function App() {
 
       <AnimatePresence mode="wait">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destination" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
+          <Route path="/space-tourism" element={<Home />} />
+          <Route path="/space-tourism/destination" element={<Destination />} />
+          <Route path="/space-tourism/crew" element={<Crew />} />
+          <Route path="/space-tourism/technology" element={<Technology />} />
         </Routes>
       </AnimatePresence>
     </>
